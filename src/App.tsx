dc6c6244266e5e7de2bfb51c8d2bda1d54678794
Max <maxi.pletenev@gmail.com>
2022-06-03 10:05:30 +0500

    Style scroll and layout for big resolutions

--- src/App.tsx
+++ src/App.tsx
@@ -1,134 +1,63 @@
 import { MenuAlt4Icon, SearchIcon } from '@heroicons/react/outline'
 
+const cat = [
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+]
+const tasks = [
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+]
+
 const App = () => {
   return (
     <div className="w-screen h-screen flex flex-col relative gap-6 bg-sky-50/50">
-      <div className="mt-8 flex items-center justify-between px-6">
+      <div className="mt-8 flex items-center justify-between px-6 sm:px-24">
         <MenuAlt4Icon className="w-8 h-8 text-slate-500" />
         <SearchIcon className="w-8 h-8 text-slate-500" />
       </div>
-      <div className="flex flex-col pl-4">
+      <div className="flex flex-col pl-4 sm:pl-24 sm:pr-24">
         <span className="text-sm text-slate-500 font-medium tracking-wider mb-2 pl-2">
           CATEGORIES
         </span>
         <div className="flex-1 w-full overflow-x-auto pl-2">
-          <div className="inline-flex gap-4 items-center pb-6">
-            <div className="w-48 h-full flex flex-col p-4 pt-6 rounded-2xl bg-white shadow-md shadow-slate-400">
-              <span className="text-sm text-slate-400 font-bold tracking-wide">
-                40 tasks
-              </span>
-              <span className="text-xl font-bold tracking-wide">Business</span>
-              <div className="w-full h-1 mt-4 rounded-md bg-slate-200 relative">
-                <div className="w-1/2 h-full bg-purple-500 rounded-md absolute top-0 left-0">
-                  <div className="h-2 w-1 bg-purple-500 absolute right-0 bottom-0 rounded-t-lg rounded-br-lg" />
+          <div className="inline-flex items-center pb-6">
+            {cat.map((el, index) => (
+              <div
+                key={'c' + index}
+                className="w-48 h-full mr-4 flex flex-col p-4 pt-6 rounded-2xl bg-white shadow-md shadow-slate-400"
+              >
+                <span className="text-sm text-slate-400 font-bold tracking-wide">
+                  40 tasks
+                </span>
+                <span className="text-xl font-bold tracking-wide">
+                  Business
+                </span>
+                <div className="w-full h-1 mt-4 rounded-md bg-slate-200 relative">
+                  <div className="w-1/2 h-full bg-purple-500 rounded-md absolute top-0 left-0">
+                    <div className="h-2 w-1 bg-purple-500 absolute right-0 bottom-0 rounded-t-lg rounded-br-lg" />
+                  </div>
                 </div>
               </div>
-            </div>
-
-            <div className="w-48 h-full flex flex-col p-4 pt-6 rounded-2xl bg-white shadow-md shadow-slate-400">
-              <span className="text-sm text-slate-400 font-bold tracking-wide">
-                40 tasks
-              </span>
-              <span className="text-xl font-bold tracking-wide">Business</span>
-              <div className="w-full h-1 mt-4 rounded-md bg-slate-200 relative">
-                <div className="w-1/2 h-full bg-purple-500 rounded-md absolute top-0 left-0">
-                  <div className="h-2 w-1 bg-purple-500 absolute right-0 bottom-0 rounded-t-lg rounded-br-lg" />
-                </div>
-              </div>
-            </div>
-
-            <div className="w-48 h-full flex flex-col p-4 pt-6 rounded-2xl bg-white shadow-md shadow-slate-400">
-              <span className="text-sm text-slate-400 font-bold tracking-wide">
-                40 tasks
-              </span>
-              <span className="text-xl font-bold tracking-wide">Business</span>
-              <div className="w-full h-1 mt-4 rounded-md bg-slate-200 relative">
-                <div className="w-1/2 h-full bg-purple-500 rounded-md absolute top-0 left-0">
-                  <div className="h-2 w-1 bg-purple-500 absolute right-0 bottom-0 rounded-t-lg rounded-br-lg" />
-                </div>
-              </div>
-            </div>
-
-            <div className="w-48 h-full flex flex-col p-4 pt-6 rounded-2xl bg-white shadow-md shadow-slate-400">
-              <span className="text-sm text-slate-400 font-bold tracking-wide">
-                40 tasks
-              </span>
-              <span className="text-xl font-bold tracking-wide">Business</span>
-              <div className="w-full h-1 mt-4 rounded-md bg-slate-200 relative">
-                <div className="w-1/2 h-full bg-purple-500 rounded-md absolute top-0 left-0">
-                  <div className="h-2 w-1 bg-purple-500 absolute right-0 bottom-0 rounded-t-lg rounded-br-lg" />
-                </div>
-              </div>
-            </div>
-
-            <div className="w-48 h-full mr-4 flex flex-col p-4 pt-6 rounded-2xl bg-white shadow-md shadow-slate-400">
-              <span className="text-sm text-slate-400 font-bold tracking-wide">
-                40 tasks
-              </span>
-              <span className="text-xl font-bold tracking-wide">Business</span>
-              <div className="w-full h-1 mt-4 rounded-md bg-slate-200 relative">
-                <div className="w-1/2 h-full bg-purple-500 rounded-md absolute top-0 left-0">
-                  <div className="h-2 w-1 bg-purple-500 absolute right-0 bottom-0 rounded-t-lg rounded-br-lg" />
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
-      <div className="flex flex-col flex-1">
-        <span className="text-sm text-slate-500 font-medium tracking-wider mb-4  px-6">
+      <div className="flex flex-col flex-1 sm:pr-16">
+        <span className="text-sm text-slate-500 font-medium tracking-wider mb-4 px-6 sm:px-24">
           TODAYS TASKS
         </span>
         <div className="h-0 w-full pb-4 flex-auto overflow-y-auto">
           <div className="flex flex-col gap-4">
-            <div className="mx-6 rounded-xl grid grid-cols-task items-center gap-4 p-4 bg-white shadow-md shadow-slate-400">
-              <div className="h-6 w-6 border-2 border-purple-500 rounded-full" />
-              <span className="w-full break-words text-sm">
-                Task text content
-              </span>
-            </div>
-            <div className="mx-6 rounded-xl grid grid-cols-task items-center gap-4 p-4 bg-white shadow-md shadow-slate-400">
-              <div className="h-6 w-6 border-2 border-purple-500 rounded-full" />
-              <span className="w-full break-words text-sm">
-                Task text content
-              </span>
-            </div>
-            <div className="mx-6 rounded-xl grid grid-cols-task items-center gap-4 p-4 bg-white shadow-md shadow-slate-400">
-              <div className="h-6 w-6 border-2 border-purple-500 rounded-full" />
-              <span className="w-full break-words text-sm">
-                Task text content
-              </span>
-            </div>
-            <div className="mx-6 rounded-xl grid grid-cols-task items-center gap-4 p-4 bg-white shadow-md shadow-slate-400">
-              <div className="h-6 w-6 border-2 border-purple-500 rounded-full" />
-              <span className="w-full break-words text-sm">
-                Task text content
-              </span>
-            </div>
-            <div className="mx-6 rounded-xl grid grid-cols-task items-center gap-4 p-4 bg-white shadow-md shadow-slate-400">
-              <div className="h-6 w-6 border-2 border-purple-500 rounded-full" />
-              <span className="w-full break-words text-sm">
-                Task text content
-              </span>
-            </div>
-            <div className="mx-6 rounded-xl grid grid-cols-task items-center gap-4 p-4 bg-white shadow-md shadow-slate-400">
-              <div className="h-6 w-6 border-2 border-purple-500 rounded-full" />
-              <span className="w-full break-words text-sm">
-                Task text content
-              </span>
-            </div>
-            <div className="mx-6 rounded-xl grid grid-cols-task items-center gap-4 p-4 bg-white shadow-md shadow-slate-400">
-              <div className="h-6 w-6 border-2 border-purple-500 rounded-full" />
-              <span className="w-full break-words text-sm">
-                Task text content
-              </span>
-            </div>
-            <div className="mx-6 rounded-xl grid grid-cols-task items-center gap-4 p-4 bg-white shadow-md shadow-slate-400">
-              <div className="h-6 w-6 border-2 border-purple-500 rounded-full" />
-              <span className="w-full break-words text-sm">
-                Task text content
-              </span>
-            </div>
+            {tasks.map((task, index) => (
+              <div
+                key={'t' + index}
+                className="mx-6 sm:ml-24 sm:mr-6 rounded-xl grid grid-cols-task items-center gap-4 p-4 bg-white shadow-md shadow-slate-400"
+              >
+                <div className="h-6 w-6 border-2 border-purple-500 rounded-full" />
+                <span className="w-full break-words text-sm">
+                  Task text content
+                </span>
+              </div>
+            ))}
           </div>
         </div>
       </div>
